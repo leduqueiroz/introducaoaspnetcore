db42884e7e2a8f1e18af228f5c73ea4bd2d28276
Eduardo Queiróz <luiz.eduardo.queiroz@outlook.com>
2019-09-12 16:06:53 -0300

    finalização encontro entityframeworkcore

---
+++ netcore-entityframeworkcore/src/Parking.Sln/Parking.Domain/Maps/AssociateMap.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parking.Domain.Maps
+{
+    public class AssociateMap : IEntityTypeConfiguration<Associate>
+    {
+        public void Configure(EntityTypeBuilder<Associate> builder)
+        {
+            builder.ToTable("Associate");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Quantity).IsRequired();
+            builder.HasOne(x => x.Customer);
+            builder.HasOne(x => x.Agreement);
+            builder.HasOne(x => x.Rate);
+        }
+    }
+}
