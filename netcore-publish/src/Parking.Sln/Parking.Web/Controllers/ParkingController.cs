27f462c5f199df84c85d29790e38fc5da815e8dd
Eduardo Queiróz <luiz.eduardo.queiroz@outlook.com>
2019-09-26 16:21:25 -0300

    finalização da aula 5

---
+++ netcore-publish/src/Parking.Sln/Parking.Web/Controllers/ParkingController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Parking.Application.Interface;
+using Parking.Dto;
+
+namespace Parking.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParkingController : ControllerBase
+    {
+        private IParkingAppService _parkingAppService { get; set; }
+
+        public ParkingController(IParkingAppService parkingAppService)
+        {
+            _parkingAppService = parkingAppService;
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]ParkingDto parkingDto)
+        {
+            if (_parkingAppService.Create(parkingDto))
+                return Ok(parkingDto);
+            else
+                return BadRequest();
+        }
+
+        [ResponseCache(CacheProfileName = "AllParking", Duration = 3600)]
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            return Ok(_parkingAppService.GetAll());
+        }
+
+        [ResponseCache(CacheProfileName = "ParkingId", Duration = 3600)]
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            return Ok(_parkingAppService.GetById(id));
+        }
+
+        [HttpGet("getallwithdapper")]
+        public IActionResult GetAllWithDapper(int id)
+        {
+            return Ok(_parkingAppService.GetAllWithDapper());
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            if (_parkingAppService.Delete(id))
+                return Ok();
+            else
+                return BadRequest();
+        }
+    }
+}
---
+++ netcore-publish/src/Parking.Sln/Parking.Web/Startup.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Parking.Application;
+using Parking.Domain;
+using Parking.Infra;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Parking.Web
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().AddFluentValidation().
+                SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDomain();
+            services.AddApplication();
+            services.AddInfra();
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new Info { Title = "Parking API", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Parking API");
+            });
+        }
+    }
+}
